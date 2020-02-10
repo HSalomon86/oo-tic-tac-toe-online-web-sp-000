175ac07984e7137d0d2ad472b0397449bbd526fc
Hendrick Salomon <hsalomon86@gmail.com>
2020-02-10 14:23:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,7 +36,7 @@ class TicTacToe
     !(@board[index].nil? || @board[index] == " ")
   end
 
-  def valid_move?(board, index)
+  def valid_move?(index)
   board = @board
   if !(position_taken?(board, index)) && on_board?(index)
     return true
